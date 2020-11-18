1dfa64e075cefdfaf8d47074cdd34e216a65114d
ErkaTsy <37425081+ErkaTsy@users.noreply.github.com>
2020-11-17 22:52:08 -0500

    created api for weather call, -created listener event on click button to get and save user's input, -added input and button elements in index.html;  added some scss files

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+// create a namespace object to represent app
+// const weatherApp = {};
 const app = {};
+// store api key within app object
+app.apiKey = 'b60c0d3d756074360b47925c6dd50cb8';
+// store the root endpoint of the API within app object
+app.api = 'https://api.openweathermap.org/data/2.5/weather';
+app.cityName = "Toronto";
 
+// global variable
+// let submitBtn = document.getElementById("userInputBtn");
 // Weather Forecast
-app.weatherForecast = function() {
+app.weatherForecast = function(cityname) {
     $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&units=metric&appid=b60c0d3d756074360b47925c6dd50cb8",
+        url: app.api,
         dataType: "json",
-        method: "GET"
+        method: "GET",
+        data:{
+            q: app.cityName,
+            appid: app.apiKey           
+        }
     }).then(function (response) {
-        console.log(response);
+        console.log(response,"mycode");
     });
 }
 
@@ -22,20 +35,34 @@ app.quotes = function () {
     });
 };
 
-//Get the main object, search by a city.
-app.init = function () {
-    $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=b60c0d3d756074360b47925c6dd50cb8",
-        dataType: "json",
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-        app.quotes();
-        app.weatherForecast();
-    });
-};
 
+// add event listener that takes user's input by pressing "enter" key
+app.addEventListener = function () {
+    let cityInput = document.getElementById("userCityInput");
+    // listen the event if user hit the "enter" key
+    cityInput.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            // call function that saves user's input
+            app.getUserInput();
+        }
+    })
+}
+
+//add event listener that calls function when user clicks button 
+app.getUserInput = () =>{
+    // return user's input and save in variable
+    app.cityName = document.getElementById("userCityInput").value;
+    // call function app.weatherForecast
+    app.weatherForecast(app.cityName);
+}
+
+
+//define a method which will initialize the app once the document is ready
+app.init = function () {
+    app.weatherForecast()
+};
 
 
 //Document Ready
 $(() => app.init());
+
